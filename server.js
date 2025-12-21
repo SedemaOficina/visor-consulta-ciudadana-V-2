57105447fe9ee8cc6c619b66879204c2e0331d0f
SedemaOficina <oficinasecretaria.sedema@gmail.com>
2025-12-20 22:33:19 -0600

    eeeee

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3005;
+
+const MIME_TYPES = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.ico': 'image/x-icon',
+};
+
+http.createServer((req, res) => {
+    // Parse URL to ignore query strings
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+    let filePath = '.' + parsedUrl.pathname;
+
+    console.log(`Request: ${req.url} -> File: ${filePath}`);
+
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const extname = path.extname(filePath);
+    let contentType = MIME_TYPES[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                res.writeHead(404);
+                res.end('404 Not Found');
+            } else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+
+}).listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
